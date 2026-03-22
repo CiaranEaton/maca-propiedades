9df0d3078093163df45871032692c6ea8c06a70d
Javier Alfonso Bustamante <71999609+CiaranEaton@users.noreply.github.com>
2026-03-22 18:41:59 -0500

    Import PropertyModal component in PropertyGallery

--- maca-propiedades/frontend/src/components/PropertyGallery.js
+++ maca-propiedades/frontend/src/components/PropertyGallery.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropertyCard from './PropertyCard';
 import { Filter, ChevronLeft, ChevronRight } from 'lucide-react';
+import PropertyModal from './PropertyModal';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
